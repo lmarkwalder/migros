36fe907d07cbe1cb2f2d2d1f4b12adc9e45d5415
Lawrence Markwalder <markwlaw@students.zhaw.ch>
2020-09-17 14:24:17 +0200

    Initial commit

---
+++ app/src/components/index.js
@@ -0,0 +1,32 @@
+import NavBar from './NavBar.svelte';
+import SideBar from './SideBar.svelte';
+
+import PageNotFound from './PageNotFound.svelte';
+import Redirect from './Redirect.svelte';
+
+
+import MigrosData from './migros-data.svelte';
+import MigrosProductSearch from './migros-product-search.svelte';
+
+import CustomVision from './azure-custom-vision.svelte';
+import FormRecognizer from './azure-form-recognizer.svelte';
+import Chatbot from './azure-chatbot.svelte';
+import BingMaps from './azure-bing-maps.svelte';
+import SpatialAnchors from './azure-spatial-anchors.svelte';
+
+
+
+
+export {
+  NavBar,
+  SideBar,
+  PageNotFound,
+  Redirect,
+  MigrosData,
+  MigrosProductSearch,
+  CustomVision,
+  FormRecognizer,
+  Chatbot,
+  BingMaps,
+  SpatialAnchors
+};
